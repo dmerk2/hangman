ee90620b6bd9cada56e6c3641e6e36031fb3e771
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-06-22 03:39:49 -0400

    added more js

--- assets/js.js
+++ assets/js.js
@@ -1,6 +1,10 @@
 //alert('Guess the Word!')
 
-var losses = 0
+var intervalId;
+var lettersCorrect = [];
+var lettersInCorrect = [];
+var timeRemaining = 60;
+var losses = 0;
 var wins = 0;
 var userGuess = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var guessesLeft = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -10,17 +14,33 @@ document.onkeyup = function(event) {
 		
 	//determines what key was selected and makes it lowercase	
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	//Start timer
+	setTimeout(timer, 60 * 1000);
+	intervalId = setInterval(showTimeLeft, 1000);
+
 };
 
 	//if user guesses correctly and than there is an alert		
-	if (userGuess == guessesLeft) {
+	if (userGuess == guessesSoFar) {
 		alert("Winner");
 		wins = wins + 1;
 		$('wins').innerHTML = wins;
 		$('losses').innerHTML = '';
-		$('guessesLeft').innerHTML = '0';
+		$('guessesSoFar').innerHTML = '0';
 
 	} else {
- 		guessesLeft = guessesLeft - 1;
-	 	$('guessesLeft').innerHTML = guessesLeft;
+ 		guessesSoFar = guessesSoFar - 1;
+	 	$('guessesSoFar').innerHTML = guessesSoFar;
 	 	$('userGuess').innerHTML += userGuess + ', ';
+
+	//end time remaining
+	function showTimeLeft(){
+		timeRemaining--;
+		$('#timer').html(timeRemaining);
+	if (timeRemaining === 0){
+		clearInterval(intervalId);
+
+	};
+};
+	
